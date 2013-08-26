579344fa29a0f8554fa8ffae09c6af02f7b13812
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-26 11:09:24 -0400

    fixture 0.1.5

--- test/fixtures/depends-0.1.5/depends-0.1.5.js
+++ test/fixtures/depends-0.1.5/depends-0.1.5.js
@@ -1,7 +1,7 @@
-// depends-0.1.0.js
+// depends-0.1.5.js
 
 var common = require('common');
 
 var go = module.exports = function () {
-  console.log('depends-0.1.0 - common', common());  
+  console.log('depends-0.1.5 - common', common());  
 };
