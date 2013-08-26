b8d5df7ca32a51ad8158f08546bb5205b69afecb
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-26 14:34:07 -0400

    trying to intercept browserify pack stream

---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict';
+var through = require('through');
+var Deduper = require('deduper');
+
+var browserify = require('browserify');
+var entry = require.resolve('./test/fixtures/');
+var deduper = new Deduper();
+
+var bfy = browserify().require(entry, { entry: true });
+var replaceDeps = [];
+
+bfy.on('package', function (file, pack) {
+  if (pack && Object.keys(pack).length) {
+    var dd = deduper.dedupe('major', file, pack);
+    
+    if (dd.replacesId) {
+      replaceDeps.push({ id: dd.replacesId, withId: file });
+    } else if (dd.id !== file) {
+      replaceDeps.push({ id: file, withId: dd.id });
+    }
+  }
+});
+
+function inspect(obj, depth) {
+  console.log(require('util').inspect(obj, false, depth || 5, true));
+}
+
+var bfy_pack = bfy.pack.bind(bfy);
+bfy.pack = function (debug, standalone) {
+  var deps = [];
+  var pack = bfy_pack(debug, standalone);
+
+  // this never calls my write and/or end methods, but instead just goes straight to the pack stream
+  return through(write, end).pipe(pack);
+
+  // this does wtf?
+  //return through(write, end);
+
+  
+  function write (d) { deps.push(d); }
+  function end () {
+    inspect(replaceDeps);
+
+    deps.forEach(function (d) {
+      this.queue(d);
+    }.bind(this));
+    this.queue(null);
+  }
+};
+
+bfy.bundle(function (err, res) {
+  if (err) return console.error(err);
+  console.log(res);
+});
---
+++ test/fixtures/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var d_0_1_0 = require('./depends-0.1.0');
+var d_0_1_5 = require('./depends-0.1.5');
+var d_0_2_0 = require('./depends-0.2.0');
+var d_1_0_0 = require('./depends-1.0.0');
+
+d_0_1_0(); 
+d_0_1_5(); 
+d_0_2_0(); 
+d_1_0_0(); 
