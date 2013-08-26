4280ecba96c2a1563c8e63898d2ecc6a236c445a
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-26 18:22:39 -0400

    working sample with callback and stream return, all or nothing dedupe

--- index.js
+++ index.js
@@ -1,38 +1,56 @@
 'use strict';
 var Deduper = require('deduper');
-
 var browserify = require('browserify');
-var entry = require.resolve('./test/fixtures/');
-var deduper = new Deduper();
+var through = require('through');
 
-var bfy = browserify().require(entry, { entry: true });
+browserify().constructor.prototype.dedupeBundle = function (opts, cb) {
+  opts = opts || {};
+  var deduper = new Deduper();
+  var criteria = opts.dedupeCriteria || 'any';
+  var passThru = through();
+  var self = this;
 
-bfy.on('package', function (file, pack) {
-  if (pack && Object.keys(pack).length) {
-    deduper.dedupe('any', file, pack);
-  }
-});
+  var deduping;
+  self
+    .on('package', function (file, pack) {
+      if (pack && Object.keys(pack).length) {
+        deduper.dedupe(criteria, file, pack);
+      }
+    })
+    .bundle(opts, function (err, res) {
+      if (deduping) return;
+      if (err) return console.error('error', err);
 
-function inspect(obj, depth) {
-  console.log(require('util').inspect(obj, false, depth || 5, true));
-}
+      deduping = true;
 
-var secondPass = false;
-bfy.bundle(function (err, res) {
-  if (secondPass) return;
+      Object.keys(deduper.cache)
+        .forEach(function (k) {
+          var deps = deduper.cache[k];
+          var dep = deps[0];
+          if (deps.length > 1) {
+            var versions = deps.map(function (d) {
+              return d.pack.version;
+            }).join(', ');
+            return self.emit('cannot-dedupe', dep.pack.name, versions);
+          }
 
-  if (err) return console.error(err);
+          self.require(dep.id, { expose: dep.pack.name });
+        });
 
-  Object.keys(deduper.cache)
-    .forEach(function (k) {
-      var dep = deduper.cache[k][0];
-      if (dep.length > 1) return console.warn('cannot dedupe %s b/c I found incompatible versions', dep[0].pack.name);
-      bfy.emit('deduping', dep);
-      bfy.require(dep.id, { expose: dep.pack.name });
+      self.bundle(opts, cb).pipe(passThru);
     });
 
-  secondPass = true;
-  bfy.bundle(function (err, res) {
-    eval(res)
+    return passThru;
+};
+
+var entry = require.resolve('./test/fixtures/');
+browserify()
+  .require(entry, { entry: true })
+  .on('cannot-dedupe', function (d, versions) { 
+    console.warn('cannot dedupe "%s" because I found incompatible versions: ', d, versions); 
+  })
+  .dedupeBundle({ dedupeCriteria: 'any' }, function (err, res) {
+    eval(res);
   });
-});
+ /*.dedupeBundle()
+ .pipe(process.stdout);*/
