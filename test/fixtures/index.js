7878c39312d26cae189b0576a8a1732a18413c2f
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-26 22:57:43 -0400

    adding a dependent to one of the common packages to test that main files are not replaced with file they depend on

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -1,11 +1,13 @@
 'use strict';
 
 var d_0_1_0 = require('./depends-0.1.0');
+var ad_0_1_0 = require('./also-depends-0.1.0');
 var d_0_1_5 = require('./depends-0.1.5');
 var d_0_2_0 = require('./depends-0.2.0');
 var d_1_0_0 = require('./depends-1.0.0');
 
 d_0_1_0(); 
+ad_0_1_0(); 
 d_0_1_5(); 
 d_0_2_0(); 
 d_1_0_0(); 
